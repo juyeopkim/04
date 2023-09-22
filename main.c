8d7ca09451d83c70d6f7169d2effa89e27fabd61
juyeopkim <71051589+juyeopkim@users.noreply.github.com>
2023-09-22 16:21:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+  int a, b;
+  
+  printf("input two integers : ");
+  scanf("%i %i", &a, &b);
+  
+  printf("%i + %i = %i \n", a, b, a+b);
+  printf("%i - %i = %i \n", a, b, a+b);
+  printf("%i * %i = %i \n", a, b, a+b);
+  printf("%i / %i = %i \n", a, b, a+b);
+  printf("%i %% %i = %i \n", a, b, a+b);
   
   system("PAUSE");	
   return 0;
