457f33e28ab9dfdf159430f346e3fcc9f11854a2
juyeopkim <71051589+juyeopkim@users.noreply.github.com>
2023-09-22 16:32:05 +0900

    4

--- main.c
+++ main.c
@@ -3,12 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-  int a;
-  
-  printf("input seconds : ");
-  scanf("%i", &a);
-  
-  printf("%i seconds - %i:%i \n", a, a/60, a%60);
+
   
   system("PAUSE");	
   return 0;
